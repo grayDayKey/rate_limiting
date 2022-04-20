e31410b6ca15ffcdd0a28f931c0c8aadcca8c92e
grayDayKey <8sergey.vyalkin8@gmail.com>
2022-04-20 11:58:46 +0700

    basic test

--- test/rate_limiting_test.dart
+++ test/rate_limiting_test.dart
@@ -3,10 +3,21 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:rate_limiting/rate_limiting.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+
+  test('RateLimiting can process routines without exceeding max attempts in period of time', () async {
+    int callsCount = 0;
+    const maxAttempts = 2;
+    const duration = Duration(milliseconds: 100);
+    final rateLimiting = RateLimiting(maxAttempts: maxAttempts, duration: duration);
+    
+    for (int i = 0; i < maxAttempts + 1; i++) {
+      rateLimiting.process(() {
+        callsCount++;
+      });
+    }
+    expect(callsCount, maxAttempts);
+    await Future.delayed(duration);
+    expect(callsCount, maxAttempts + 1);
+    rateLimiting.cancel();
   });
 }
